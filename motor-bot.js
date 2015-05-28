56e3c7ce731cadb94814c4ec35fc60c4195d50a5
Stuart Knightley <stuart@stuartk.com>
2015-05-28 15:13:03 -0400

    Support reverse

--- motor-bot.js
+++ motor-bot.js
@@ -18,6 +18,14 @@ MotorBot.prototype.move = function move(x, y) {
   var starboardSpeed = y;
 
   console.log("portSpeed", portSpeed, "starboardSpeed", starboardSpeed);
-  this.portMotor.start(portSpeed);
-  this.starboardMotor.start(starboardSpeed);
+  if (portSpeed >= 0) {
+    this.portMotor.start(portSpeed);
+  } else {
+    this.portMotor.reverse(-portSpeed);
+  }
+  if (starboardSpeed >= 0) {
+      this.starboardMotor.start(starboardSpeed);
+  } else {
+      this.starboardMotor.reverse(-starboardSpeed);
+  }
 };
